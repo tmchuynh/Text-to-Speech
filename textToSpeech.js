bf63956bcec49bd74c80e296172226e8c39fc1ff
Tina Huynh <tinamchuynh@gmail.com>
2022-09-29 11:02:44 -0700

    volume

--- textToSpeech.js
+++ textToSpeech.js
@@ -1,1 +1,12 @@
 let speech = new SpeechSynthesisUtterance();
+speech.lang = "en";
+
+document.querySelector("#talk").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+});
+
+document.querySelector("#rate").addEventListener("input", () => {
+    const rate = document.querySelector("#rate").value;
+    speech.rate = rate;
+    document.querySelector("#rate-label").innerHTML = rate;
+});
